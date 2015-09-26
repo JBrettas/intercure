780103a6ae3050527c08ad8bb608dcc1288c6350
Julio Brettas <juliobrettas@gmail.com>
2015-09-26 20:31:35 -0300

    Improved readability and commented inter_frailty function

---
+++ R/intercure.R
@@ -0,0 +1,12 @@
+#' intercure: Cure rate regression models for interval censored data.
+#'
+#' The intercure package provides three main functions: inter_mixture, inter_bch
+#' and inter_frailty. These are essentialy algorithms for estimating
+#' the cure fraction with mixture, promotion time or frailty model, respectively.
+#'
+#' @section intercure functions:
+#' inter_mixture, inter_bch, inter_frailty
+#'
+#' @docType package
+#' @name intercure
+NULL
