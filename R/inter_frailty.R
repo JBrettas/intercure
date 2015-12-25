407632205cbd24f294656cea66d5bfa4f348dfb0
JULIO BRETTAS <juliobrettas@gmail.com>
2015-12-25 14:22:08 -0200

    rotulos variable changed to lbls

--- R/inter_frailty.R
+++ R/inter_frailty.R
@@ -226,12 +226,12 @@ inter_frailty <- function(dataset, left, right, delta,
   compr_theta <- 1 + length(cov_theta); compr_beta <- length(cov_beta)
   compr_alpha <- compr_theta + compr_beta
   a_M <- matrix(NA, nrow=M, ncol=compr_alpha)
-  rotulos <- c("intercept", cov_theta, cov_beta)
-  colnames(a_M) <- rotulos
+  lbls <- c("intercept", cov_theta, cov_beta)
+  colnames(a_M) <- lbls
   theta_M <- matrix(a_M[,1:compr_theta], nrow=M)
-  colnames(theta_M) <- rotulos[1:compr_theta]
+  colnames(theta_M) <- lbls[1:compr_theta]
   beta_M <- matrix(a_M[, (compr_theta + 1):compr_alpha], nrow=M)
-  colnames(beta_M) <- rotulos[(compr_theta + 1):compr_alpha]
+  colnames(beta_M) <- lbls[(compr_theta + 1):compr_alpha]
   alpha <- c(1:compr_alpha) * 0
   sigma_alpha <- b * diag(compr_alpha)
   beta <- alpha[(compr_theta + 1):compr_alpha]
