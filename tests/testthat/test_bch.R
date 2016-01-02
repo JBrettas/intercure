8ca35e83e72bfb74c3088a54d5ea16ab26bb7ea0
Julio Brettas <juliobrettas@gmail.com>
2016-01-02 17:19:40 -0200

    updates unit tests and creates unit tests for inter_frailty_cl

--- tests/testthat/test_bch.R
+++ tests/testthat/test_bch.R
@@ -25,4 +25,22 @@ test_that("results doesn't change", {
   expect_equal(fitx2$par, c(-0.7927699,  0.2740729), tolerance = 0.0001)
 })
 
+# stops when covariate names are wrong
+test_that("stops when covariate names are wrong", {
+  expect_error(inter_bch(data_test_frailty, data_test_frailty$L, data_test_frailty$R, "ok"))
+  expect_error(inter_bch(data_test_frailty, data_test_frailty$L, data_test_frailty$R, c("ok","xi1")))
+})
 
+# stops when negative times are found
+test_that("stops when negative times are found", {
+  expect_error(inter_bch(data_test_frailty, -data_test_frailty$L, data_test_frailty$R, "xi1"))
+  L_neg <- data_test$L
+  L_neg[1] <- -5
+  expect_error(inter_bch(data_test_frailty, L_neg, data_test_frailty$R, "xi1"))
+  expect_error(inter_bch(data_test_frailty, data_test_frailty$L, -data_test_frailty$R, "xi1"))
+})
+
+# stops if L > R
+test_that("stops if L > R", {
+  expect_error(inter_bch(data_test_frailty, data_test_frailty$R, data_test_frailty$L, "xi1"))
+})
--- tests/testthat/test_frailty.R
+++ tests/testthat/test_frailty.R
@@ -2,7 +2,7 @@ context("inter_frailty")
 
 set.seed(30)
 data_test <- sim_frailty(30)
-
+delta2 <- data_test$delta + 1
 
 # one covariate
 fit <- suppressWarnings(inter_frailty(data_test, data_test$L, data_test$R, data_test$delta, c("xi1"), c("xi1"), M = 10, max_n = 5, burn_in = 0))
@@ -37,22 +37,37 @@ test_that("parallel mode is working", {
 
 snow::stopCluster.default(cl)
 
-
 # M = 1 gives error
 test_that("M = 1 gives error", {
   expect_error(suppressWarnings(inter_frailty(data_test, data_test$L, data_test$R, data_test$delta, c("xi1"), c("xi1"), M = 1, max_n = 5, burn_in = 0)))
 })
-
 
 # M = 250 works
 test_that("works for M = 250", {
   expect_is(suppressWarnings(inter_frailty(data_test, data_test$L, data_test$R, data_test$delta, c("xi1"), c("xi1"), M = 250, max_n = 5, burn_in = 0))$par, "numeric")
 })
 
+# stops on non 0 or 1 delta
+test_that("throws error if delta is not binary", {
+  expect_error(inter_frailty(data_test, data_test$L, data_test$R, delta2, c("xi1"), c("xi1"), M = 10, max_n = 5, burn_in = 0))
+})
 
-# error if delta contains different of 1 or 0
-# test_that("delta contains different from 1 or 0", {
-#   expect_is(inter_frailty(data_test, data_test$L, data_test$R, (1+data_test$delta), c("xi1"), c("xi1"), M = 250, max_n = 5, burn_in = 1)$par, "numeric")
-# })
+# stops when covariate names are wrong
+test_that("stops when covariate names are wrong", {
+  expect_error(suppressWarnings(inter_frailty(data_test, data_test$L, data_test$R, data_test$delta, c("ok"), c("xi1"), M = 1, max_n = 5, burn_in = 0)))
+  expect_error(suppressWarnings(inter_frailty(data_test, data_test$L, data_test$R, data_test$delta, c("xi1"), c("wrong"), M = 1, max_n = 5, burn_in = 0)))
+})
 
+# stops when negative times are found
+test_that("stops when negative times are found", {
+  expect_error(suppressWarnings(inter_frailty(data_test, -data_test$L, data_test$R, data_test$delta, c("ok"), c("xi1"), M = 1, max_n = 5, burn_in = 0)))
+  L_neg <- data_test$L
+  L_neg[1] <- -5
+  expect_error(suppressWarnings(inter_frailty(data_test, L_neg, data_test$R, data_test$delta, c("xi1"), c("wrong"), M = 1, max_n = 5, burn_in = 0)))
+  expect_error(suppressWarnings(inter_frailty(data_test, data_test$L, -data_test$R, data_test$delta, c("xi1"), c("wrong"), M = 1, max_n = 5, burn_in = 0)))
+})
 
+# stops if L > R
+test_that("stops if L > R", {
+  expect_error(suppressWarnings(inter_frailty(data_test, data_test$R, data_test$L, data_test$delta, c("ok"), c("xi1"), M = 1, max_n = 5, burn_in = 0)))
+})
